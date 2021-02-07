0a4f1ff4fe0cd9aa2222d76f09e9bf68cb471d02
deepakgarg22396 <gargdeepak22396@gmail.com>
2021-02-07 22:44:54 +0530

    Initial Commit: Adding Task tracker using react with json server as a backend

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button style={{ backgroundColor: color}} className='btn' onClick = {onClick}>
+            {text}
+        </button>
+    )
+
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.protoTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
